08d370d3001b9f27d569083f5a7f14a1a96ebc08
mh <mh@immerda.ch>
2008-06-13 18:52:39 +0000

    merged with puzzle

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,21 @@
 # puppet module
+# modules/puppet/manifests/init.pp - manage puppet stuff
 # original by luke kanies
 # http://github.com/lak
-# adaapted by puzzel itc
+# adapted by puzzel itc
 # merged with immerda project group's
 # solution
-#######################################
+#
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # modules_dir { "puppet": }
 
