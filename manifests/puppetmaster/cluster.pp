47b1850555111378d61640a12f22aa4331e2c51a
mh <mh@immerda.ch>
2008-07-02 20:32:07 +0000

    adding requirements, not overwrite

--- manifests/puppetmaster/cluster.pp
+++ manifests/puppetmaster/cluster.pp
@@ -8,7 +8,7 @@ class puppet::puppetmaster::cluster inherits puppet::puppetmaster {
     }
 
     File[puppet_config] {
-        require => [ Package[mongrel], Package[nginx], File[nginx_config] ],
+        require +> [ Package[mongrel], Package[nginx], File[nginx_config] ],
     }
 
     case $operatingsystem {
