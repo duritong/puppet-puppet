9db87a60590dfe439e782c5651d118f9324f396a
mh <mh@immerda.ch>
2008-07-26 11:16:39 +0000

    added debian config file

--- manifests/init.pp
+++ manifests/init.pp
@@ -85,7 +85,14 @@ class puppet::gentoo inherits puppet::linux {
     }
 }
 class puppet::debian inherits puppet::linux {
-    # there is really no status cmd for it
+    file{'/etc/default/puppet':
+        source => [ "puppet://$server/files/puppet/client/debian/${fqdn}/puppet",
+                    "puppet://$server/files/puppet/client/debian/${domain}/puppet",
+                    "puppet://$server/files/puppet/client/debian/puppet",
+                    "puppet://$server/puppet/client/debian/puppet" ],
+        notify => Service[puppet],
+        owner => root, group => 0, mode => 0644;
+    }    # there is really no status cmd for it
     Service[puppet]{
         hasstatus => false,
     }
