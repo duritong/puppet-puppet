fd2ed55bb5ac75e15da78900335929e4417802d9
mh <mh@immerda.ch>
2008-12-27 21:41:39 +0000

    syntax check

--- manifests/puppetmaster/checklastrun.pp
+++ manifests/puppetmaster/checklastrun.pp
@@ -3,10 +3,10 @@ class puppet::puppetmaster::checklastrun {
     file{'/opt/bin/puppetlast':
         source => [ "puppet://$server/files/puppet/master/puppetlast",
                     "puppet://$server/puppet/master/puppetlast"],
-        owner => root, group => 0, mode =0700;
+        owner => root, group => 0, mode => 0700;
     }
     file{'/etc/cron.d/puppetlast.cron':
-        content => "40 10,22 * * * root /opt/bin/puppetlast"
+        content => "40 10,22 * * * root /opt/bin/puppetlast",
         require => File['/opt/bin/puppetlast'],
         owner => root, group => 0, mode => 0644;
     }
