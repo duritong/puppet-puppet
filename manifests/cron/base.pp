7e441bf983263011763fccdcc6307e2805ccf1f0
mh <mh@immerda.ch>
2008-12-27 17:01:00 +0000

    fix test cmd

--- manifests/cron/base.pp
+++ manifests/cron/base.pp
@@ -6,6 +6,6 @@ class puppet::cron::base inherits puppet::base {
     }
     exec{'stop_puppet':
         command => 'kill `cat /var/run/puppet/puppetd.pid`',
-        onlyif => 'test -f test -f /var/run/puppet/puppetd.pid',
+        onlyif => 'test -f /var/run/puppet/puppetd.pid',
     }
 }
