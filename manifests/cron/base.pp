b3c1d8d45f46f6ce521c23a135117a5216704449
mh <mh@immerda.ch>
2009-10-27 14:22:42 +0100

    fix disabling of the service
    - don't use the status of the service script
    - try to kill puppet _after_ we managed the service

--- manifests/cron/base.pp
+++ manifests/cron/base.pp
@@ -3,9 +3,11 @@
 class puppet::cron::base inherits puppet::base {
     Service['puppet']{
         enable => false,
+        hasstatus => false,
     }
     exec{'stop_puppet':
         command => 'kill `cat /var/run/puppet/puppetd.pid`',
         onlyif => 'test -f /var/run/puppet/puppetd.pid',
+        require => Service['puppet'],
     }
 }
