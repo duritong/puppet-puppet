9e54a92395046dde9a588e3d8e80ded99745ade2
varac <varacanero@zeromail.org>
2010-01-20 14:08:29 +0100

    changed destination from /opt/bin/puppetlast to /usr/local/bin

--- manifests/puppetmaster/checklastrun.pp
+++ manifests/puppetmaster/checklastrun.pp
@@ -1,20 +1,12 @@
 class puppet::puppetmaster::checklastrun {
-    case $operatingsystem {
-         debian,ubuntu: { $puppetlast_dest = "/usr/local/bin/puppetlast" }
-         default: { $puppetlast_dest = "/opt/bin/puppetlast" }
-    }
-
-
-
-
-    file{"$puppetlast_dest":
+    file{"/usr/local/bin/puppetlast":
         source => [ "puppet://$server/modules/site-puppet/master/puppetlast",
                     "puppet://$server/modules/puppet/master/puppetlast"],
         owner => root, group => 0, mode => 0700;
     }
     file{'/etc/cron.d/puppetlast.cron':
-        content => "40 10,22 * * * root $puppetlast_dest\n",
-        require => File["$puppetlast_dest"],
+        content => "40 10,22 * * * root /usr/local/bin/puppetlast\n",
+        require => File["/usr/local/bin/puppetlast"],
         owner => root, group => 0, mode => 0644,
 	notify => service["crond"];
     }
