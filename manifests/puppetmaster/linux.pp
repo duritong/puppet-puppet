dfdb96c1c071b817d9ccc50cd2e4790a36a78ec7
nadir <nadir-technik@nadir.org>
2010-02-17 14:25:28 +0100

    On puppet::puppetmaster::passenger apache2 needs to be restarted instead of puppetmasted

--- manifests/puppetmaster/linux.pp
+++ manifests/puppetmaster/linux.pp
@@ -2,20 +2,27 @@
 
 class puppet::puppetmaster::linux inherits puppet::linux {
     
-if defined (puppet::puppetmaster::passenger) {
-	$puppetmaster_servicename = "puppetmaster" }
-    else {
-	$puppetmaster_servicename = "apache2"
-        }
-
-
-    service{'puppetmaster':
-        ensure => running,
-        name => $puppetmaster_servicename,
-	enable => true,
-        require => [ Package[puppet] ],
+    if defined (puppet::puppetmaster::passenger) {
+	service{'puppetmaster':
+	    ensure => running,
+	    #name => apache2,
+	    #enable => true,
+	    pattern => 'apache2',
+	    hasstatus => true,
+	    start => '/etc/init.d/apache2 start',
+	    stop => '/etc/init.d/apache2 start',
+	    restart => '/etc/init.d/apache2 restart',
+	    status => 'pgrep apache2',
+	    require => [ Package[puppet] ],
+	}
     }
-
+    else {
+	service{'puppetmaster':
+	    ensure => running,
+	    enable => true,
+	    require => [ Package[puppet] ],
+	}
+    }
     Service[puppet]{
         require +> Service[puppetmaster],
     }
