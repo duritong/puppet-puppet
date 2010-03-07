23c385e39e89ffd326bf12ad2d7b1640d4f65a95
nadir <nadir-technik@nadir.org>
2010-03-07 22:56:02 +0100

    README, added $puppet_crontime to tune how often a client gets updates

--- manifests/cron/linux.pp
+++ manifests/cron/linux.pp
@@ -4,9 +4,15 @@ class puppet::cron::linux inherits puppet::linux {
     case $puppet_config {
         '': { $puppet_config = '/etc/puppet/puppet.conf' }
     }
+    
+    case $puppet_crontime {
+        '': { $puppet_crontime = '0,30 * * * *' }
+    }
+
+
     File['/etc/cron.d/puppetd.cron']{
         source => undef,
         content => "# run puppet
-0,30 * * * * root /usr/sbin/puppetd --onetime --no-daemonize --splay --config=$puppet_config --color false | grep -E '(^err:|^alert:|^emerg:|^crit:)'\n",
+$puppet_crontime root /usr/sbin/puppetd --onetime --no-daemonize --splay --config=$puppet_config --color false | grep -E '(^err:|^alert:|^emerg:|^crit:)'\n",
     }
 }
