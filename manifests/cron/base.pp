291c1b7ebd093c421da2be7906030ce8caa2dffd
mh <mh@immerda.ch>
2010-08-19 11:31:24 +0200

    work around a 2.6 issue

--- manifests/cron/base.pp
+++ manifests/cron/base.pp
@@ -10,11 +10,19 @@ class puppet::cron::base inherits puppet::base {
       #it's already disabled
     }
     default: {
-      Service['puppet']{
-        hasstatus => false,
+      $puppet_majorversion = regsubst($puppetversion,'^(\d+\.\d+).*$','\1')
+      if $puppet_majorversion == '2.6' {  
+        Service['puppet']{
+          ensure => stopped,
+        }
+      } else {
+        Service['puppet']{
+          hasstatus => false,
+        }
       }
     }
   }
+  # this works only on < 2.6
   exec{'stop_puppet':
     command => 'kill `cat /var/run/puppet/puppetd.pid`',
     onlyif => 'test -f /var/run/puppet/puppetd.pid',
