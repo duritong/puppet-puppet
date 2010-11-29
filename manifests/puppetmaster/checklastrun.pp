5e24e0e85d354300078f0b10b80cbc44a0f76f10
mh <mh@immerda.ch>
2010-11-29 23:23:10 +0100

    replace puppetlast with much more improved version

--- manifests/puppetmaster/checklastrun.pp
+++ manifests/puppetmaster/checklastrun.pp
@@ -1,11 +1,22 @@
 class puppet::puppetmaster::checklastrun {
-  file{"/usr/local/bin/puppetlast":
-    source => [ "puppet:///modules/site-puppet/master/puppetlast",
-                "puppet:///modules/puppet/master/puppetlast" ],
+  $puppet_lastruncheck_ignorehosts_str = $puppet_lastruncheck_ignorehosts ? {
+    '' => '',
+    undef => '',
+    default => "--ignore-hosts ${puppet_lastruncheck_ignorehosts}"
+  }
+
+  $puppet_lastruncheck_timeout_str = $puppet_lastruncheck_timeout ? {
+    '' => '',
+    undef => '',
+    default => "--timeout ${puppet_lastruncheck_timeout}"
+  }
+
+  file{"/usr/local/sbin/puppetlast":
+    source => [ "puppet:///modules/puppet/master/lastruncheck" ],
     owner => root, group => 0, mode => 0700;
   }
   file{'/etc/cron.d/puppetlast.cron':
-    content => "${puppetmaster_checklastrun} root /usr/local/bin/puppetlast ${puppetmaster_checklastrun_timeout}\n",
+    content => "${puppetmaster_lastruncheck_cron} root /usr/local/sbin/puppetlast ${puppet_lastruncheck_timeout_str} ${puppet_lastruncheck_ignorehosts_str} ${$puppet_lastruncheck_additionaloptions}\n",
     require => File["/usr/local/bin/puppetlast"],
     owner => root, group => 0, mode => 0644,
   }
--- manifests/puppetmaster/checklastrun/disable.pp
+++ manifests/puppetmaster/checklastrun/disable.pp
@@ -1,5 +1,6 @@
 class puppet::puppetmaster::checklastrun::disable inherits puppet::puppetmaster::checklastrun {
-  File['/usr/local/bin/puppetlast']{
+  File['/usr/local/sbin/puppetlast']{
+    source => undef,
     ensure => absent,
   }
   File['/etc/cron.d/puppetlast.cron']{
