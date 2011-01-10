7bc44c94a94e772542bb69476f306c3d1aef90ff
Micah Anderson <micah@riseup.net>
2011-01-10 18:20:46 -0500

    add to puppet::puppetmaster::debian a file resource to manage the /etc/default/puppetmaster file shipped with the debian package

--- manifests/puppetmaster/debian.pp
+++ manifests/puppetmaster/debian.pp
@@ -3,4 +3,13 @@ class puppet::puppetmaster::debian inherits puppet::puppetmaster::package {
   if $puppetmaster_mode != 'passenger' {
     Service['puppetmaster'] { hasstatus => true, hasrestart => true }
   }
+
+  file { '/etc/default/puppetmaster':
+    source => [ "puppet:///modules/site-puppet/master/debian/${fqdn}/puppetmaster",
+                "puppet:///modules/site-puppet/master/debian/${domain}/puppetmaster",
+                "puppet:///modules/site-puppet/master/debian/puppetmaster",
+                "puppet:///modules/puppet/master/debian/puppetmaster" ],
+    notify => Service[puppetmaster],
+    owner => root, group => 0, mode => 0644;
+  }
 }
