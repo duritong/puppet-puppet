45acc8df306666f1eaa033e519b24e261dcca7b0
intrigeri <intrigeri@boum.org>
2011-05-07 20:10:46 +0200

    Inherit from the right base class.

--- manifests/puppetmaster/package/debian.pp
+++ manifests/puppetmaster/package/debian.pp
@@ -1,4 +1,4 @@
-class puppet::puppetmaster::package::debian inherits puppet::puppetmaster::package {
+class puppet::puppetmaster::package::debian inherits puppet::puppetmaster::package::base {
 
   Package['puppetmaster']{
     require => Package['puppetmaster-common']
