f37df509cf1ea3edafd4e60eaa349add9ae19e63
intrigeri <intrigeri@boum.org>
2011-05-07 20:15:34 +0200

    Install puppetmaster-common package.
    This should fix the remaining issues from
    https://labs.riseup.net/code/issues/2928

--- manifests/puppetmaster/package/debian.pp
+++ manifests/puppetmaster/package/debian.pp
@@ -1,5 +1,9 @@
 class puppet::puppetmaster::package::debian inherits puppet::puppetmaster::package::base {
 
+  package { 'puppetmaster-common':
+    ensure => present,
+  }
+
   Package['puppetmaster']{
     require => Package['puppetmaster-common']
   }
