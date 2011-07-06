b3dc59ba25d05529936736297334ce86b402445f
mh <mh@immerda.ch>
2011-07-07 01:24:29 +0200

    implement setting reports cleanup dir the old way

--- manifests/puppetmaster/cleanup_reports.pp
+++ manifests/puppetmaster/cleanup_reports.pp
@@ -1,8 +1,8 @@
 class puppet::puppetmaster::cleanup_reports {
-
+  case $puppetmaster_reports_dir { '',undef: { $puppetmaster_reports_dir = '/var/lib/puppet/reports' } }
   # clean up reports older than $puppetmaster_cleanup_reports days
   file { '/etc/cron.daily/puppet_reports_cleanup.sh':
-    content => "#!/bin/bash\nfind /var/lib/puppet/reports/ -maxdepth 2 -type f -ctime +${puppetmaster_cleanup_reports} -exec rm {} \\;\n",
+    content => "#!/bin/bash\nfind ${puppetmaster_reports_dir} -maxdepth 2 -type f -ctime +${puppetmaster_cleanup_reports} -exec rm {} \\;\n",
     owner => root, group => 0, mode => 0700;
   }
 }
