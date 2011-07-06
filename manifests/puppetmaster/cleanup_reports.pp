0fd0df89f4a0bd0700b3260a4c40041c59f65660
mh <mh@immerda.ch>
2011-07-07 01:04:20 +0200

    fix variable naming

--- manifests/puppetmaster/cleanup_reports.pp
+++ manifests/puppetmaster/cleanup_reports.pp
@@ -6,7 +6,7 @@ class puppet::puppetmaster::cleanup_reports(
 
   if $cleanup_older_than != 'absent' {
     File['/etc/cron.daily/puppet_reports_cleanup.sh']{
-      content => "#!/bin/bash\nfind ${puppet::puppetmaster::cleanup_reports::reports_dir} -maxdepth 2 -type f -ctime +${puppet::puppetmaster::cleanup_reports::cleanup_olderthan} -exec rm {} \\;\n",
+      content => "#!/bin/bash\nfind ${puppet::puppetmaster::cleanup_reports::reports_dir} -maxdepth 2 -type f -ctime +${puppet::puppetmaster::cleanup_reports::cleanup_older_than} -exec rm {} \\;\n",
       owner => root, group => 0, mode => 0700,
     }
   } else {
