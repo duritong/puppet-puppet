bad29d3812a9e02a03df35f5b21feac7aa1360f6
mh <mh@immerda.ch>
2012-12-06 23:14:22 +0100

    use correct location to get logs

--- manifests/master/munin.pp
+++ manifests/master/munin.pp
@@ -3,7 +3,8 @@ class puppet::master::munin {
   munin::plugin::deploy {
     [ 'puppetmaster_memory', 'puppet_clients' ]:
       source => "puppet/munin/puppet_",
-      config => "user root"
+      config => "user root
+env.logfile /var/log/puppet/puppetmaster.log"
   }
 
 }
