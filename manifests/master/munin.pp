56c15eb79702694ecd0a413958c2e5498a794f0a
mh <mh@immerda.ch>
2012-12-07 12:56:28 +0100

    improve puppet munin plugin

--- manifests/master/munin.pp
+++ manifests/master/munin.pp
@@ -1,10 +1,13 @@
+# deploy puppet munin plugin
 class puppet::master::munin {
-
-  munin::plugin::deploy {
-    [ 'puppetmaster_memory', 'puppet_clients' ]:
-      source => "puppet/munin/puppet_",
-      config => "user root
-env.puppet_logfile /var/log/puppet/puppetmaster.log"
+  munin::plugin::deploy{'puppet_':
+    ensure => absent,
+    source => 'puppet/munin/puppet_';
   }
-
+  munin::plugin{
+    ['puppet_clients','puppet_mem']:
+      ensure  => 'puppet_',
+      require => Munin::Plugin::Deploy['puppet_'],
+      config  => 'user root';
+  }
 }
