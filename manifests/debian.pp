3e9785959978ac9a0beae4827aeae802865c9279
mh <mh@immerda.ch>
2014-06-28 19:52:22 +0200

    linting

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,12 +1,15 @@
+# debian specific things
 class puppet::debian inherits puppet::linux {
 
   file { '/etc/default/puppet':
-    source => [ "puppet:///modules/site_puppet/client/debian/${::fqdn}/puppet",
+    source  => ["puppet:///modules/site_puppet/client/debian/${::fqdn}/puppet",
                 "puppet:///modules/site_puppet/client/debian/${::domain}/puppet",
-                "puppet:///modules/site_puppet/client/debian/puppet",
-                "puppet:///modules/puppet/client/debian/puppet" ],
-    notify => Service[puppet],
-    owner => root, group => 0, mode => 0644;
+                'puppet:///modules/site_puppet/client/debian/puppet',
+                'puppet:///modules/puppet/client/debian/puppet' ],
+    notify  => Service[puppet],
+    owner   => root,
+    group   => 0,
+    mode    => '0644';
   }
 
   package{ 'puppet-common':
