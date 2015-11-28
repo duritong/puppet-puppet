1223fac5d819a34a74493238d48bfba20a069678
mh <mh@immerda.ch>
2015-11-28 17:41:41 +0100

    linting for future parser

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,11 +1,13 @@
+# manage centos specific things
 class puppet::centos inherits puppet::linux {
-
-  file { '/etc/sysconfig/puppet':
+  file{'/etc/sysconfig/puppet':
     source => [ "puppet:///modules/site_puppet/sysconfig/${::fqdn}/puppet",
                 "puppet:///modules/site_puppet/sysconfig/${::domain}/puppet",
-                "puppet:///modules/site_puppet/sysconfig/puppet",
-                "puppet:///modules/puppet/sysconfig/puppet" ],
-    notify => Service[puppet],
-    owner => root, group => 0, mode => 0644;
+                'puppet:///modules/site_puppet/sysconfig/puppet',
+                'puppet:///modules/puppet/sysconfig/puppet' ],
+    notify => Service['puppet'],
+    owner  => 'root',
+    group  => 0,
+    mode   => '0644';
   }
 }
--- manifests/master/cleanup_reports.pp
+++ manifests/master/cleanup_reports.pp
@@ -1,7 +1,9 @@
+# clean up reports older than $puppetmaster_cleanup_reports days
 class puppet::master::cleanup_reports {
-  # clean up reports older than $puppetmaster_cleanup_reports days
   file { '/etc/cron.daily/puppet_reports_cleanup.sh':
     content => "#!/bin/bash\nfind ${puppet::master::reports_dir} -maxdepth 2 -type f -ctime +${puppet::master::cleanup_reports} -exec rm {} \\;\n",
-    owner => root, group => 0, mode => 0700;
+    owner   => root,
+    group   => 0,
+    mode    => '0700';
   }
 }
